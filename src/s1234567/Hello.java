533f567d4f5479e4efd1f19c3755795939dbce66
Lee, Jiwoo <zizii23027@gmail.com>
2025-05-16 16:48:04 +0900

    two

---
+++ src/s1234567/Hello.java
@@ -0,0 +1,9 @@
+public class Hello { 
+    
+    public static int sum(int n) { 
+        int result = 0; 
+        for (int i = 1; i <= n; ++i) 
+            result += i; 
+        return result; 
+    } 
+}
--- src/s1234567/Main.java
+++ src/s1234567/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("1234567 이지우");
-
+		System.out.println(Hello.sum(10)); 
+		System.out.println(Hello.sum(100))
 	}
 
 }
