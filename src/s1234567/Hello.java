b30261877b1006fdc2cb0e58fd601e17aa81a141
Lee, Jiwoo <zizii23027@gmail.com>
2025-05-16 16:51:49 +0900

    three

--- src/s1234567/Hello.java
+++ src/s1234567/Hello.java
@@ -1,9 +1,6 @@
 public class Hello { 
     
     public static int sum(int n) { 
-        int result = 0; 
-        for (int i = 1; i <= n; ++i) 
-            result += i; 
-        return result; 
+    	return (n == 1) ? 1 : n + sum(n - 1);
     } 
 }
